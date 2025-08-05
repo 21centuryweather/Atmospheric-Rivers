683457d5bc22910846c55220bb866f36ae81b283
BinkyBo <wmarshall098@gmail.com>
2025-08-05 06:01:28 +0100

    added test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+import xarray as xr
+from REID_ARalgorithm_v3_module import REID_ARalgorithm_v3
+from scipy.io import loadmat
+import numpy as np
+
+'''try:
+    ds = xr.open_dataset('IVT_Aust_daily_2deg_ERA5_1960-2014.nc')
+    print("Dataset loaded successfully.")
+    print(ds)
+except Exception as e:
+    print(f"An error occurred while opening the dataset: {e}")'''
+
+try:
+    observedA1,observedA2,observedB = REID_ARalgorithm_v3(loadmat("ivt_3rd_timestep.mat"),250,-90,0,1,0,370,1,2000,2)
+except Exception as e:
+    print(f"Module threw error when testing:{e}")
+    exit()
+
+try:
+    expectedA1 = np.swapaxes(loadmat("A1.mat")["A1"], 0, 1)
+    expectedA2 = np.swapaxes(loadmat("A2.mat")["A2"], 0, 1)
+    expectedY = loadmat("Y.mat")["Y"]
+    expectedB = loadmat("B.mat")["B"]
+    #print(expectedB[0].dtype)
+    #exit(0)
+except Exception as e:
+    print(f"Eception while loading expected data{e}")
+    exit()
+
+print("started testing")
+#print(type(expectedA1),type(observedA1))
+if np.array_equal(expectedA1, observedA1):
+    print("A1 same")
+else:
+    print(f"A1 test failed\nexpected:{expectedA1}\nobserved:{observedA1}")
+
+if np.array_equal(expectedA2, observedA2,equal_nan=True):
+    print("A2 same")
+else:
+    print(f"A2 test failed\nexpected:{expectedA2}\nobserved:{observedA2}")
+
+if np.array_equal(expectedB[0], observedB):
+    print("B same")
+else:
+    print(f"B test failed\nexpected:\nobserved:{observedB}")
+#Centroid,MajorAxisLength,MinorAxisLength,Orientation,PixelList,MeanIntensity,MaxIntensity
