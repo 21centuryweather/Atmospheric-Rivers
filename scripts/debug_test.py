075e41e77c57b783250221f06920f3b84e97ec9a
BinkyBo <wmarshall098@gmail.com>
2025-08-06 02:58:54 +0100

    Cleaning up unneeded code

--- scripts/debug_test.py
+++ scripts/debug_test.py
@@ -30,29 +30,17 @@ if __name__ == "__main__":
     length_threshold=2000
     aspect_ratio=2
     dataset = inp['f']
+
     lat=[i for i in range(lati,latf,lat_res)]
     lon=[i for i in range(loni,lonf,lon_res)]
-
-    mask_temp=np.zeros_like(dataset)
+    
     #create binary mask based on input threshold
-    A1=np.ones(dataset.ndim)
-    A2=dataset
-
     A1 = dataset>IVT_threshold
-    A2[dataset<IVT_threshold]=np.nan
 
-
-    regions = measure.regionprops(measure.label(A1),dataset)#,properties=('centroid', 'orientation', 'axis_major_length', 'axis_minor_length'))
+    regions = measure.regionprops(measure.label(A1),dataset)
 
     # Convert centroid coords from pixelspace to lat/lon space
 
-    coords = []
-    for region in regions:
-        lat_c = lat[round(region.centroid[0])]
-        lon_c = lon[round(region.centroid[1])]
-        coords.append([lat_c,lon_c])
-
-    AR_lengths =  []
     for region in regions:
         L = region.major_axis_length/2 * ((lat_res + lon_res)/2.)
         region.lat_c = lat[round(region.centroid[0])]
@@ -60,35 +48,40 @@ if __name__ == "__main__":
 
         a2 = region.lon_c + L*np.sin(region.orientation)
         
-        arc=acosd(round(sind(region.lon_c)*\
-                          sind(a2)+cosd(region.lon_c)*\
-                          (cosd(a2)*cosd(L*cosd(region.orientation))),15))#remove rounding errors
+        arc=acosd(sind(region.lon_c)*sind(a2)+\
+                  cosd(region.lon_c)*(cosd(a2)*cosd(L*cosd(region.orientation))))
         
         AR_length=2*RADIUS_EARTH*arc*np.pi/180/1000.
         region.AR_length = AR_length
-        AR_lengths.append(AR_length)
-        coords.append([region.lat_c,region.lon_c])
     
     #length of river must exceed...
     regions = [region for region in regions if region.AR_length>length_threshold]
 
-    #aspect ratio and orientation angle test. Excludes systems within 5 degrees of equator (mostly artifacts)
-    regions = [region for region in regions if region.axis_major_length/region.axis_minor_length>=aspect_ratio and abs(region.lat_c) >5]
+    #aspect ratio test.
+    regions = [region for region in regions if region.axis_major_length/region.axis_minor_length>=aspect_ratio]
     
+    #orientation angle test. Excludes systems within 5 degrees of equator (mostly artifacts)
+    regions = [region for region in regions if abs(region.lat_c) >5]
+
     #Orientation angle is just to get rid of artifacts
     regions = [region for region in regions if abs(np.rad2deg(region.orientation))>10]
 
     #maps pixels in the AR onto lat-lon array
+    mask=np.zeros_like(dataset)
     for region in regions:
         for pixel in region.coords:
-            mask_temp[pixel[0]][pixel[1]] = 1#dataset[pixel[0]][pixel[1]]
+            mask[pixel[0]][pixel[1]] = 1
+
+
+
+    
     import matplotlib.pyplot as plt
     fig, axes = plt.subplots(1, 2)
 
-    axes[0].imshow(mask_temp, cmap='jet')
+    axes[0].imshow(mask, cmap='jet')
 
     inp=loadmat('data/mask.mat')
     axes[1].imshow(inp['mask'], cmap='jet')
-    
+
     plt.tight_layout()
     plt.show()
