d3d54de2a67f47733bcd10782ae9d6a0d614875e
pgregory <pgregory@unimelb.edu.au>
2025-08-07 13:53:44 +1000

    UPDATES : Have debug.py working using relative path and appending to           sys.path

--- scripts/debug.py
+++ scripts/debug.py
@@ -3,18 +3,28 @@ from skimage import measure
 import xarray as xr
 from scipy.io import loadmat 
 from geopy.distance import great_circle
-import os
-os.chdir("..")
+from pathlib import Path
+import sys
+
+# Find the project root directory and then add it to sys.path
+pwd = Path(__file__).parent
+PROJECT_DIR = pwd.parent 
+
+#print (pwd)
+sys.path.insert(0,str(PROJECT_DIR))
+#print (sys.path)
 
 from atmospheric_rivers.find_rivers import Identify_AR
-
 import matplotlib.pyplot as plt
-mask = Identify_AR()
+
+DATA_DIR = PROJECT_DIR / 'data'
+
+mask = Identify_AR(PATH= DATA_DIR / 'IVT_input_slice.nc')
 fig, axes = plt.subplots(1, 2)
 
 axes[0].imshow(mask, cmap='jet')
 
-inp=loadmat('data/mask.mat')
+inp=loadmat( DATA_DIR / 'mask.mat')
 axes[1].imshow(inp['mask'], cmap='jet')
 
 plt.tight_layout()
