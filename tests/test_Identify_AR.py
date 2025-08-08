32d6fcc2a3f2881d8ce0902838195541e2059c2c
BinkyBo <wmarshall098@gmail.com>
2025-08-08 16:09:38 +1000

    UPDATE undid some changes made in fix

--- tests/test_Identify_AR.py
+++ tests/test_Identify_AR.py
@@ -16,7 +16,7 @@ def test_config(pytestconfig):
     DATA_DIR = Path(pytestconfig.rootdir / 'data')
     print(f'DATA_DIR = {DATA_DIR}')
 
-def test_region_props(pytestconfig):
+def test_Identify_AR(pytestconfig):
     """
     Run the skimage.measure regionprops module against a set of input data
     This test is valid for skimage version 0.25.2
@@ -28,7 +28,7 @@ def test_region_props(pytestconfig):
     mask = Identify_AR(DATA_PATH)
 
 
-    LOG.info(f'Mask was {len(mask.size)}')
+    LOG.info(f'Mask was {mask.size}')
 
     """
     Load expected data
@@ -36,5 +36,33 @@ def test_region_props(pytestconfig):
     from scipy.io import loadmat
     expected =loadmat('data/mask.mat')["mask"]
 
+    # Write code using assert statements so the regions you have loaded are what you expect
+    assert np.array_equal(mask, expected,equal_nan=True)
+
+def test_Identify_AR_version(pytestconfig):
+    """
+    Run the skimage.measure regionprops module against a set of input data
+    This test is valid for skimage version 0.25.2
+    """
+    LOG.info(f'testconfig.rootdir = {pytestconfig.rootdir}')
+    DATA_DIR = Path(pytestconfig.rootdir / 'data')
+    
+    DATA_PATH = DATA_DIR / 'IVT_input_slice.nc'
+    mask = Identify_AR(DATA_PATH)
+    
+    # use
+    # mask.tofile(DATA_DIR/"IVT_input_slice8.8.25.bin")
+    # to store the file
+
+    LOG.info(f'Mask was {mask.size}')
+
+    """
+    Load expected data
+    """
+    from scipy.io import loadmat
+    
+    expected =np.fromfile(DATA_DIR/"IVT_input_slice8.8.25.bin",dtype = np.float64)
+    expected = expected.reshape(mask.shape)
+    
     # Write code using assert statements so the regions you have loaded are what you expect
     assert np.array_equal(mask, expected,equal_nan=True)
--- tests/test_skimage.py
+++ tests/test_skimage.py
@@ -23,7 +23,7 @@ def test_region_props(pytestconfig):
 
 
     LOG.info(f'Found {len(regions)} regions')
-
+    regions = np.array([[r.centroid[1]+1,r.centroid[0]+1,r.axis_major_length,r.axis_minor_length,[np.rad2deg(r.orientation),90+np.rad2deg(r.orientation)][r.orientation<0],r.mean_intensity,r.max_intensity] for r in regions])
     """
     Load expected data
     """
@@ -31,4 +31,18 @@ def test_region_props(pytestconfig):
     expected =loadmat('data/B.mat')["B"]
 
     # Write code using assert statements so the regions you have loaded are what you expect
-    assert np.array_equal(expected, regions,equal_nan=True)
+    expected = np.array([ [float(r[0][0][0]),float(r[0][0][1]),float(r[1][0][0]),float(r[2][0][0]),[float(r[3][0][0]),90+float(r[3][0][0])][float(r[3][0][0])<0],float(r[5][0][0]),float(r[6][0][0])] for r in expected])
+    expected=expected[expected[:, 0].argsort()]
+    regions=regions[regions[:, 0].argsort()]
+
+    Zero_expected = [e for e in expected if np.isclose(e[3],1.15470054)]
+    Zero_regions = [r for r in regions if np.isclose(r[3],0)]
+    Zero_expected = [[e[0],e[1],e[5],e[6]] for e in Zero_expected]
+    Zero_regions = [[r[0],r[1],r[5],r[6]] for r in Zero_regions]
+    assert np.allclose(Zero_expected, Zero_regions, equal_nan=True,rtol=0.1)
+
+    expected = [e for e in expected if not np.isclose(e[3],1.15470054)]
+    regions = [r for r in regions if not np.isclose(r[3],0)]
+
+    np.set_printoptions(suppress=True)
+    assert np.allclose(expected, regions, equal_nan=True,rtol=0.1)
